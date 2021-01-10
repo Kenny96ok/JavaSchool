7b2e17169d52d7e998c639f7d05060ec8074e449
Kenny96ok <kenny96k@gmail.com>
2021-01-10 21:36:08 +0300

    First try

---
+++ src/HomeWork1/Main.java
@@ -0,0 +1,56 @@
+package HomeWork1;
+
+import java.io.Console;
+
+public class Main {
+
+    public static void printMas(int mas[])
+    {
+        for (int elem:mas) {
+            System.out.print(elem+" ");
+        }
+        System.out.println();
+    }
+    public static void bubbleSorter(int mas[])
+    {
+        for (int out = mas.length- 1; out >= 1; out--)
+        {
+            for (int in = 0; in < out; in++)
+            {
+                if(mas[in] > mas[in + 1]) {
+                    int temp = mas[in];
+                    mas[in] = mas[in + 1];
+                    mas[in + 1] = temp;
+                }
+            }
+        }
+    }
+    public static int binSearch(int mas[], int k)
+    {
+        int i = -1;
+        if (mas != null) {
+            int low = 0, high = mas.length, mid;
+            while (low < high) {
+                mid = (low + high) >>> 1;
+                if (k == mas[mid]) {
+                    i = mid;
+                    break;
+                } else {
+                    if (k < mas[mid]) {
+                        high = mid;
+                    } else {
+                        low = mid + 1;
+                    }
+                }
+            }
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+        int[] mas={2,5,4,3,1};
+        printMas(mas);
+        bubbleSorter(mas);
+        printMas(mas);
+        System.out.println("Искомый элемент под индексом " + binSearch(mas,5));
+    }
+}
