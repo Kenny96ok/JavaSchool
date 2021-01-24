7cf45d61978f6a6b563eb58a828a76583bb3182f
Kenny96ok <kenny96k@gmail.com>
2021-01-24 16:39:58 +0300

    Домашка по колекциям: задача про авто

---
+++ src/HomeWork2/Main.java
@@ -0,0 +1,48 @@
+package HomeWork2;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static class Car
+    {
+        public String model;
+        public String type;
+        public Car(String model,String type)
+        {
+            this.model=model;
+            this.type=type;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        //Имеется список парка машин Car(String model, String type).
+        // Необходимо разбить его на списки сгруппированные по type.
+        List<Car> list = new ArrayList<Car>();
+        FileReader reader = new FileReader(".//src//HomeWork2//file.txt");
+        Scanner scanner = new Scanner(reader);
+        while (scanner.hasNext()) {
+            list.add(new Car(scanner.next(), scanner.next()));
+        }
+        reader.close();
+
+        HashMap<String, List<String>> map = new HashMap();
+        for (Car car : list) {
+            List<String> temp = map.get(car.type);
+            if (temp == null) {
+                temp = new ArrayList<>(1);
+            }
+            temp.add(car.model);
+            map.put(car.type, temp);
+        }
+        for (String type : map.keySet()) {
+            System.out.print(type + ": ");
+            for (String model : map.get(type)) {
+                System.out.print(model + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
